817f1994d32084329405e9c75eeacd34c4371ee5
s-fx <fxscholler@protonmail.com>
2021-11-23 11:11:52 +0100

    init project

---
+++ src/main/java/com/futureSheep/ApplicationMS_kbe/ApplicationMsKbeApplication.java
@@ -0,0 +1,13 @@
+package com.futureSheep.ApplicationMS_kbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMsKbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMsKbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futureSheep/ApplicationMS_kbe/ApplicationMsKbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureSheep.ApplicationMS_kbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMsKbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
