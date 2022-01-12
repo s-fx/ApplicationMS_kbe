11b639d4ee8ff285de8e1f2c15bb1331c8867dd7
KatOTB <kurlei@web.de>
2022-01-12 18:05:51 +0100

    added openapi documentation (swagger)

--- src/main/java/com/futureSheep/ApplicationMS_kbe/controller/GoogleController.java
+++ src/main/java/com/futureSheep/ApplicationMS_kbe/controller/GoogleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/external")
+@RequestMapping("/api")
 public class GoogleController {
 
     @Autowired
