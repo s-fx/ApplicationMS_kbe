7f2b84e9a724e245113fb01792d239b3f4639d0f
s-fx <fxscholler@protonmail.com>
2022-01-12 18:58:44 +0100

    logger

--- src/main/java/com/futureSheep/ApplicationMS_kbe/controller/LaptopController.java
+++ src/main/java/com/futureSheep/ApplicationMS_kbe/controller/LaptopController.java
@@ -9,8 +9,6 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
